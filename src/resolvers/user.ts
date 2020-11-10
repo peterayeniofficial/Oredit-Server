0d04b328241bde4bd848c0261b2f8923509721ca
Peter Ayeni <peter.ayeni@previsico.com>
2020-11-10 20:41:42 +0000

    feat: add session

---
+++ src/resolvers/user.ts
@@ -0,0 +1,132 @@
+import { Arg, Ctx, Field, InputType, Mutation, Resolver, ObjectType, Query } from 'type-graphql';
+import { MyContext } from '../types';
+import { User } from '../entities/User';
+import argon2 from 'argon2'
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string 
+  @Field()
+  password: string
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string
+  @Field()
+  message: string
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], {nullable: true})
+  errors?: Error[]
+
+  @Field(() => User, {nullable: true})
+  user?: User
+}
+
+@Resolver()
+export class UserResolver {
+  @Query(() => User, {nullable: true})
+  async me(
+    @Ctx() {req, em}: MyContext
+  ){
+    // not login
+    if(!req.session.userId){
+      return null
+    }
+
+    const user = em.findOne(User, {id: req.session.userId})
+    return user
+  }
+
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg('options') options: UsernamePasswordInput,
+    @Ctx() {em, req}: MyContext
+  ): Promise<UserResponse>{
+
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+          field: "username",
+          message: "Username is too short, must be greater than 2"
+        },
+      ],
+      }
+    }
+
+    if (options.password.length <= 2) {
+      return {
+        errors: [
+          {
+          field: "password",
+          message: "Username is too short, must be greater than 2"
+        },
+      ],
+      }
+    }
+
+    const hashedPassword = await argon2.hash(options.password)
+    const user = em.create(User, {username: options.username, password: hashedPassword})
+
+    try {
+      await em.persistAndFlush(user)
+
+    } catch (error) {
+      if(error.code === "23505" || error.detail.includes("already exists")){
+        return {
+          errors: [
+            {
+              field: "username",
+              message: "username already taken"
+            }
+          ]
+        }
+      }
+      console.log("message: ", error.message)
+    }
+    // store user id session
+    // this will set a cookie on the user
+    // kep them logged in
+    req.session.userId = user.id
+    return {user}
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg('options') options: UsernamePasswordInput,
+    @Ctx() {em, req}: MyContext
+  ): Promise<UserResponse>{
+    const user = await em.findOne(User, {username: options.username})
+    if (!user) {
+      return {
+        errors: [
+          {
+          field: "username",
+          message: "That username does not exist"
+        },
+      ],
+      }
+    }
+    const valid = await argon2.verify(user.password, options.password)
+    if (!valid) {
+      return {
+        errors: [
+          {
+          field: "password",
+          message: "Incorrect Password"
+        },
+      ],
+      }
+    }
+
+    req.session.userId = user.id
+
+    return {user}
+  }
+}
